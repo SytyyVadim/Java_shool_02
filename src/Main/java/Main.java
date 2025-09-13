fbf2672626103ea1a1625bad767fe45971b8181e
vadimsytyj <paraplanrowid@gmail.com>
2025-09-13 09:52:36 +0300

    java_shool_02

--- src/Main/java/Main.java
+++ src/Main/java/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 25;
+        int age = 20;
         System.out.println(age);
+
+        System.out.println(add(10,age));
+    }
+
+    public static int add(int a, int b) {
+        for (int i = 0;  i < 10; i++) {
+            b = a + b * i;
+        }
+        return  b;
     }
 }
